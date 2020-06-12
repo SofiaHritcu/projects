2b701a439bfe383814a6a829279ad60b96bb7329
SofiaHritcu <61913547+SofiaHritcu@users.noreply.github.com>
2020-06-13 01:09:30 +0300

    Add files via upload

---
+++ SwimmingContestModel/src/main/java/swimmingContest/model/SwimmingStroke.java
@@ -0,0 +1,9 @@
+package swimmingContest.model;
+
+/***
+ * the swimming stroke type
+ * the values of a swimming stroke could be : freestyle, backstroke, butterfly,mixed
+ */
+public enum SwimmingStroke {
+    freestyle, backstroke, butterfly,mixed
+}
